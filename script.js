574a5b2924049324984920f7ce383335e49f11f5
Abdulmalik-Ayorinde <malik.ayo4life@gmail.com>
2020-09-14 00:28:10 +0100

    Added new API call

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const container = document.getElementById('container')
+const searchBox = document.getElementById('search')
+const searchButton = document.getElementById('button')
 
 
-
-
-var data = fetch('http://www.omdbapi.com/?apikey=c3e4e145&s=iron-man')
+async function fetchData() {
+    container.innerHTML = ''
+await fetch(`http://www.omdbapi.com/?apikey=c3e4e145&s=${searchBox.value}`)
 .then(resp => resp.json())
 .then(data => {
-    var info = data.Search
+    const info = data.Search
 
     info.forEach(result => {
-    console.log(result)
+        fetch(`http://www.omdbapi.com/?apikey=c3e4e145&t=${result.Title}`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        }) 
+
+
+        // functionality for the first API call
         var img = document.createElement('img')
         img.setAttribute('src', `${result.Poster}`)
         container.appendChild(img)
         var details = document.createElement('div')
-        details.innerHTML = `<li class="year">${result.Year}</li>
-        <li class=""></li>
-        <li class="plot"></li>`
+        details.innerHTML = `<li class="">${result.Title}</li>
+        <li class="year">${result.Year}</li>
+        <li class="type">${result.Type}</li>
+        <li class="id">${result.imdbID}</li>`
         details.classList.add('details')
         container.appendChild(details)
+   
 })
+
 })
+
+
+}
+
+searchButton.addEventListener('click', fetchData)
