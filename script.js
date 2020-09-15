996ce204330d479d003df00e3f88465ee80cec0f
Abdulmalik-Ayorinde <malik.ayo4life@gmail.com>
2020-09-15 08:30:00 +0100

    Refactoring for easy readability

--- script.js
+++ script.js
@@ -11,30 +11,29 @@ await fetch(`http://www.omdbapi.com/?apikey=c3e4e145&s=${searchBox.value}`)
     const info = data.Search
 
     info.forEach(result => {
-        fetch(`http://www.omdbapi.com/?apikey=c3e4e145&t=${result.Title}`)
+        fetch(`http://www.omdbapi.com/?apikey=c3e4e145&t=${result.Title}&plot=full`)
         .then(resp => resp.json())
-        .then(data => {
-            console.log(data)
+        .then(secondCallData => {
+            console.log(secondCallData)
+            putInDOm(result, secondCallData)
         }) 
 
+})
+})
+}
 
+function putInDOm(result, secondCallData) {
         // functionality for the first API call
+        var details = document.createElement('div')
         var img = document.createElement('img')
         img.setAttribute('src', `${result.Poster}`)
-        container.appendChild(img)
-        var details = document.createElement('div')
+        details.appendChild(img)
         details.innerHTML = `<li class="">${result.Title}</li>
         <li class="year">${result.Year}</li>
         <li class="type">${result.Type}</li>
-        <li class="id">${result.imdbID}</li>`
+        <li class="id">${secondCallData.Plot}</li>`
         details.classList.add('details')
         container.appendChild(details)
-   
-})
-
-})
-
-
 }
 
 searchButton.addEventListener('click', fetchData)
